92dcece83164bead45167f58736f3d5dbbf6a3e0
fnc314 <fnc314@gmail.com>
2014-01-21 22:02:33 -0800

    All but nav-bar challenge complete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BogApp::Application.config.secret_key_base = '726c073c8e3100c18a4a64595bf642fea6377206d4bdc086c831149cd92a2f34699333b51a2e3b4d4941a60af87e12d98032cc3c6b443fcd7e545fd4305960f7'
